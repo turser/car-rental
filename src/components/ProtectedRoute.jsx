24e42a594e09905272d59860a08ed397aabde67d
Yasser EL KADDOURI <133793500+turser@users.noreply.github.com>
2026-07-05 01:15:47 +0100

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children }) {
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-    return isAuthenticated ? children : <Navigate to="/login" replace />;
-}
