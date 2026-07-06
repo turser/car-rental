6740e0ae10d9d59e370baf1d8aae6aa76b51ed52
hamzabousakoura <bousakoura999@gmail.com>
2026-07-06 22:17:16 +0100

    update model rental

--- backend/app/Models/Rental.php
+++ backend/app/Models/Rental.php
@@ -10,43 +10,48 @@ class Rental extends Model
     use HasFactory;
 
     protected $fillable = [
-        'client_id' , 
-        'car_id' , 
-        'agency_id' , 
-        'start_date' , 
-        'end_date' , 
-        'actual_return_date' , 
+        'client_id',
+        'car_id',
+        'agency_id',
+        'start_date',
+        'end_date',
+        'actual_return_date',
         'price_per_day',
-        'total_price', 
-        'paid_amount', 
+        'total_price',
+        'paid_amount',
         'status',
-    ] ;
+    ];
     public function client()
-{
-    return $this->belongsTo(Client::class);
-}
+    {
+        return $this->belongsTo(Client::class);
+    }
 
-public function agency()
-{
-    return $this->belongsTo(Agency::class);
-}
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
 
-public function car()
-{
-    return $this->belongsTo(Car::class);
-}
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
 
-public function services()
-{
-    return $this->belongsToMany(
-        Service::class,
-        'rental_services'
-    )
-    ->withPivot([
-        'quantity',
-        'unit_price',
-        'total_price'
-    ])
-    ->withTimestamps();
-}
+    public function services()
+    {
+        return $this->belongsToMany(
+            Service::class,
+            'rental_services'
+        )
+            ->withPivot([
+                'quantity',
+                'unit_price',
+                'total_price'
+            ])
+            ->withTimestamps();
+    }
+
+    public function extensions()
+    {
+        return $this->hasMany(RentalExtension::class);
+    }
 }
