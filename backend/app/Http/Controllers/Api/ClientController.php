c2f1e8ca2722f09416b63d784b1e354f352d7771
hamzabousakoura <bousakoura999@gmail.com>
2026-07-07 20:12:42 +0100

    update function index clientcontroller

--- backend/app/Http/Controllers/Api/ClientController.php
+++ backend/app/Http/Controllers/Api/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
